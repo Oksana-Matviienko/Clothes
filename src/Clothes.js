4abb7b923b802a7fa877b8e19bcd8e3ca86f35e7
Oksana <7564211@gmail.com>
2022-05-02 21:19:22 +0200

    first commit

---
+++ src/Clothes.js
@@ -0,0 +1,19 @@
+function Clothes({outfit}){
+   return(
+      <div className="products">
+{outfit.map((element=>{
+   const {id, name, searchTerm, price, image} = element;
+   return(
+      <div key={id} className="product-card">
+            <img src={image} width="400px" height="500px"/>
+         <div className="product-info">
+            <h3>{name}</h3>
+            <h4>$ {price}</h4>
+         </div>
+      </div>
+   )
+}))}
+      </div>
+   )
+}
+export default Clothes;
